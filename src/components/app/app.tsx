fda9b70f674acbffbd3be08a3afa0e1f9de8a8d5
vindimm <vindimm@bk.ru>
2024-04-07 20:58:52 +0500

    настроил роутинг

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,12 @@
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+
+import { AppRoute, AuthorizationStatus } from '../../const';
+import FavoritesPage from '../../pages/favorites-page/favorites-page';
+import LoginPage from '../../pages/login-page/login-page';
 import MainPage from '../../pages/main-page/main-page';
+import NotFoundPage from '../../pages/not-found-page/not-found-page';
+import OfferPage from '../../pages/offer-page/offer-page';
+import PrivateRoute from '../../components/private-route/private-route';
 
 type AppComponentProps = {
   offersCount: number;
@@ -6,7 +14,34 @@ type AppComponentProps = {
 
 function App({offersCount}: AppComponentProps): JSX.Element {
   return (
-    <MainPage offersCount={offersCount} />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Root}
+          element={<MainPage offersCount={offersCount} />}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={
+            <PrivateRoute authorizationStatus={AuthorizationStatus.Auth}>
+              <FavoritesPage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<LoginPage />}
+        />
+        <Route
+          path={AppRoute.Offer}
+          element={<OfferPage />}
+        />
+        <Route
+          path="*"
+          element={<NotFoundPage />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
